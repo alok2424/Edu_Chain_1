e210cdd0415a17dcc0e50bdf9f28603651c53982
alok2424 <22ec059@jssaten.ac.in>
2024-12-29 13:02:09 +0530

    commit

--- client/src/components/handles/Handles.jsx
+++ client/src/components/handles/Handles.jsx
@@ -6,9 +6,9 @@ import { FaGithubSquare } from 'react-icons/fa';
 const Handles = () => {
   return (
     <section className='socials'>
-      <a href="https://www.linkedin.com/in/kshitij-srivastava-4778a8151/" target='_blank' rel="noopener noreferrer"><AiFillLinkedin className='icon' /></a>
-      <a href="https://twitter.com/kshitijweb3?lang=en" target='_blank' rel="noopener noreferrer"><AiFillTwitterSquare className='icon' /></a>
-      <a href="https://github.com/kshitijofficial" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' /></a>
+      <a href="https://www.linkedin.com/in/alok-kumar-gupta-2075b5257" target='_blank' rel="noopener noreferrer"><AiFillLinkedin className='icon' /></a>
+      <a href="https://x.com/Alok679166/9" target='_blank' rel="noopener noreferrer"><AiFillTwitterSquare className='icon' /></a>
+      <a href="https://github.com/alok2424" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' /></a>
 
     </section>
   )
